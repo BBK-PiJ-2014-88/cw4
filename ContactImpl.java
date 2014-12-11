74fa4ea6b0b6e601ea4a6ff6a3df866c94c571f0
Sergio <rurounisergii@hotmail.com>
2014-12-11 19:17:25 +0000

    added minimum code so ContactImpl will compile and i can run the getID test in ContactTest

--- ContactImpl.java
+++ ContactImpl.java
@@ -1,17 +1,23 @@
 public class ContactImpl implements Contact{
+	private static int uniqueIdGenerator = 1;
 	private int id;
 	private String name;
 	private String notes;
 
+	public ContactImpl(){
+	}
 	public int getId(){
+		return 1;
 	}
 
 	public String getName(){
+		return "";
 	}
 
 	public String getNotes(){
+		return "";
 	}
 
-	void addNotes(String note){
+	public void addNotes(String note){
 	}
 }
