be9645d6cd06839dc59a7dde6d6ad7ae1e06e15c
Sergio <rurounisergii@hotmail.com>
2014-12-11 21:54:16 +0000

    Started MeetingImpl, added method names

---
+++ MeetingImpl.java
@@ -0,0 +1,12 @@
+import java.util.Calendar;
+import java.util.Set;
+
+public class MeetingImpl implements Meeting{
+	int getId(){
+		return 1;
+	}
+	Calendar getDate(){
+	}
+	Set<Contact> getContacts(){
+	}
+}
