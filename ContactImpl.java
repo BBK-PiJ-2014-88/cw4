1b9cdac9498704bf69af8c4f7e58d6adb5368b39
Sergio <rurounisergii@hotmail.com>
2014-12-11 19:09:51 +0000

    Started ContactImpl. Added empty skeleton methods and variables

---
+++ ContactImpl.java
@@ -0,0 +1,17 @@
+public class ContactImpl implements Contact{
+	private int id;
+	private String name;
+	private String notes;
+
+	public int getId(){
+	}
+
+	public String getName(){
+	}
+
+	public String getNotes(){
+	}
+
+	void addNotes(String note){
+	}
+}
