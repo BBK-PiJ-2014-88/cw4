209d3828bd625b6564eaac5001728334de26af36
Sergio <rurounisergii@hotmail.com>
2014-12-11 21:38:50 +0000

    Deleted println statements i used to check what was wrong. I think it is a bad idea to have a static variable that creates the uniqueIQ so will probably add another method for getting uniqueID when i get to the Contact Manager

--- ContactTest.java
+++ ContactTest.java
@@ -9,16 +9,12 @@ public class ContactTest{
 	@Before
 	public void buildUp(){
 		contactTester1 = new ContactImpl();
-		System.out.println(contactTester1.getId());
 		contactTester2 = new ContactImpl();
-		System.out.println(contactTester2.getId());
 		contactTester3 = new ContactImpl();
-		System.out.println(contactTester3.getId());
 	}
 
 	@Test
 	public void testFirstContactReturnCorrectID(){
-				System.out.println("calling test 1");
 		int expected = 1;
 		int output = contactTester1.getId();
 		assertEquals(expected,output);
@@ -26,7 +22,6 @@ public class ContactTest{
 
 	@Test
 	public void testReturnCorrectID(){
-		System.out.println("calling test 2");
 		int expected = 6; //3 contact instances were created in 1st test, another 3 in this test, so this is the 6th object so will have an id of 6
 		int output = contactTester3.getId();
 		assertEquals(expected, output);
@@ -34,7 +29,6 @@ public class ContactTest{
 
 	@Test
 	public void testReturnCorrectNotes(){
-				System.out.println("calling test 3");
 		contactTester1.addNotes("Notes for Testing");
 		String expected = "Notes for Testing";
 		String output = contactTester1.getNotes();
