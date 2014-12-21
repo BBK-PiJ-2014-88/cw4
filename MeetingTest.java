1e284bd49806ce5460aa0a687d44abd931059b09
Sergio <rurounisergii@hotmail.com>
2014-12-21 14:13:06 +0000

    Started work on Meeting Test

---
+++ MeetingTest.java
@@ -0,0 +1,15 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class MeetingTest{
+	private Meeting meetingTester1;
+	private Meeting meetingTester2;
+	private Meeting meetingTester3;
+	
+	@Before
+	public void buildUp(){
+		meetingTester1 = new MeetingImpl();
+		meetingTester2 = new MeetingImpl();
+		meetingTester3 = new MeetingImpl();
+	}
+}
