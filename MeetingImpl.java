79a455d1e661cf217cde1c680a579d6953d05bad
Sergio <rurounisergii@hotmail.com>
2014-12-21 14:37:52 +0000

    added junit test for getId() in MeetingImpl. fails as expected

--- MeetingImpl.java
+++ MeetingImpl.java
@@ -1,9 +1,14 @@
 import java.util.Calendar;
 import java.util.Set;
 
+private Set<Contact> contactList;
+private Calendar date;
+
 public class MeetingImpl implements Meeting{
+	public Meeting(int id, Calendar date){
+	}
 	int getId(){
-		return 1;
+		return 0;
 	}
 	Calendar getDate(){
 	}
--- MeetingTest.java
+++ MeetingTest.java
@@ -5,11 +5,18 @@ public class MeetingTest{
 	private Meeting meetingTester1;
 	private Meeting meetingTester2;
 	private Meeting meetingTester3;
-	
+
 	@Before
 	public void buildUp(){
 		meetingTester1 = new MeetingImpl();
 		meetingTester2 = new MeetingImpl();
 		meetingTester3 = new MeetingImpl();
+	}
+
+	@Test
+	public void testFirstContactReturnCorrectID(){
+		int expected = 1;
+		int output = meetingTester1.getId();
+		assertEquals(expected,output);
 	}
 }
