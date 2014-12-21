29189566eda193c81e314e169c2a6e1ec49f8f51
Sergio <rurounisergii@hotmail.com>
2014-12-21 23:39:38 +0000

    Added test for addFutureMeeting. Test failed as expected

--- ContactManagerTest.java
+++ ContactManagerTest.java
@@ -1,21 +1,32 @@
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 
 public class ContactManagerTest{
 	private ContactManager contactManagerTester1;
 	private ContactManager contactManagerTester2;
+	private Set<Contact> contacts = new TreeSet<Contact>();
+	private Set<Meeting> meetings = new TreeSet<Meeting>();
+	Calendar randomDate = new GregorianCalendar(2014,12,10);
 
 	@Before
 	public void buildUp(){
 		contactManagerTester1 = new ContactManagerImpl();
 		contactManagerTester2 = new ContactManagerImpl();
+		contacts.add(new ContactImpl("Smith"));
+		contacts.add(new ContactImpl("John"));
+		contacts.add(new ContactImpl("Steven"));
+
 	}
 
 	@Test
-	public void firstTest(){
+	public void testAddFutureMeeting(){
 		int expected = 1;
-		int output = 1;
+		int output = contactManagerTester1.addFutureMeeting(contacts,randomDate);
 		assertEquals(expected,output);
 	}
 }
