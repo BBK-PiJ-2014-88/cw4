c966b257d0d52f0156b2b58d001e65e2f8a5fd1d
Sergio <rurounisergii@hotmail.com>
2014-12-23 12:18:55 +0000

    Added another test to ContactTest to check both parameters work fine in constructor method

--- ContactTest.java
+++ ContactTest.java
@@ -5,12 +5,14 @@ public class ContactTest{
 	private Contact contactTester1;
 	private Contact contactTester2;
 	private Contact contactTester3;
+	private Contact contactTester4;
 
 	@Before
 	public void buildUp(){
 		contactTester1 = new ContactImpl("",1);
 		contactTester2 = new ContactImpl("Steven",2);
 		contactTester3 = new ContactImpl("",3);
+		contactTester4 = new ContactImpl("John", " is cool", 4);
 	}
 
 	@Test
@@ -42,6 +44,13 @@ public class ContactTest{
 		assertEquals(expected, output);
 	}
 
+	@Test
+	public void testBothParameters(){
+		String expected = "John is cool";
+		String output = contactTester4.getName() + contactTester4.getNotes();
+		assertEquals(expected, output);
+	}
 
+	// Later on, need to add test where new contact is created with same ID as another. Throws error as Id's must be unique.
 
 }
